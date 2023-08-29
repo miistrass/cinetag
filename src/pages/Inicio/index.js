afdaae36e487a3f57d5acacd37bf92cebd3dddda
miistrass <milenastrass@gmail.com>
2023-08-29 20:57:03 -0300

    criando banner, titulo, cards e lista dinâmica

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,24 @@
 import Cabecalho from "components/Cabecalho";
-import Rodape from "components/Rodape"
+import Rodape from "components/Rodape";
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
+            <Banner imagem="home"/>
+            <Titulo>
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
